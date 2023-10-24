083e3479de6815b36b07ba42161323b15cd8b59e
cballejo <christianballejo@gmail.com>
2023-10-24 12:38:25 -0300

    ejemplo de gestion de tratamiento

---
+++ practicas/tto.R
@@ -0,0 +1,72 @@
+library(tidyverse)
+
+guess_encoding("practicas/DATOSTRATAMIENTOS-TRATAMIENTOS-2023926.csv")
+
+tto <- read_csv2("practicas/DATOSTRATAMIENTOS-TRATAMIENTOS-2023926.csv",
+                 locale = locale(encoding = "ISO-8859-1"), na = c("", "N/D"))
+
+glimpse(tto)
+
+tto <- tto |>  mutate(FITTO = dmy(FITTO),
+                      FINTO = dmy(FINTO))
+
+summary(tto$FITTO)
+
+tto |> 
+  count(IDPTE, IDTUM, IDTTO, sort = T)
+
+tto |> 
+  count(IDPTE, IDTUM, sort = T)
+
+tto |> 
+  count(IDPTE, sort = T)
+
+idtum <- tto |> 
+  count(IDPTE, IDTUM) |> 
+  filter(n > 1) |>  pull(IDTUM)
+
+trat <- tto |>  filter(IDTUM %in% idtum)
+
+summary(trat$FITTO)
+
+trat |> 
+  select(IDPTE, IDTUM, IDTTO, FITTO, FINTO, ESTTON, TPGFN) |> 
+  arrange(IDPTE, IDTUM, IDTTO)
+
+# cantidad de estrategias por tumor/paciente
+
+tto |> 
+  distinct(ESTTON)
+
+tto |> 
+  select(IDPTE, IDTUM, IDTTO, FITTO, FINTO, ESTTON, TPGFN) |> 
+  filter(!is.na(ESTTON) & ESTTON != "Ignorado") |> 
+  count(IDPTE, IDTUM, ESTTON) |> 
+  pivot_wider(names_from = ESTTON, values_from = n, values_fill = 0) |> 
+  View()
+
+# tiempos promedio de cada tto segun topografia
+# antes se debería agrupar la topografia
+
+tto |> 
+  filter(FITTO > "1900-01-01" & FINTO > "1900-01-01") |> 
+  mutate(tiempo = interval(FITTO, FINTO)/ddays(),
+         año_inicio = year(FITTO),
+         mes_inicio = month(FITTO)) |> 
+  select(IDPTE, IDTUM, IDTTO, tiempo, año_inicio, mes_inicio, ESTTON, TPGFN) |> 
+  filter(!is.na(tiempo) & tiempo >= 0 & !is.na(ESTTON)) |> 
+  group_by(TPGFN, ESTTON) |> 
+  summarise(tiempo = mean(tiempo)) |> 
+  arrange(desc(tiempo))
+
+
+
+tto |> 
+  mutate(tiempo = interval(FITTO, FINTO)/ddays(),
+         año_inicio = year(FITTO),
+         mes_inicio = month(FITTO),
+         inicio = str_glue("{año_inicio}-{mes_inicio}")) |> 
+  select(IDPTE, IDTUM, IDTTO, tiempo, año_inicio, mes_inicio, inicio, ESTTON, TPGFN) |> 
+  filter(!is.na(tiempo) & tiempo >= 0 & !is.na(ESTTON) & año_inicio == 2023) |> 
+  arrange(inicio) |> 
+  pivot_wider(names_from = inicio, values_from = ESTTON) |> View()
