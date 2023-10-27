bbf39d7f54b61f89a4b7e9005aa99ecdccae5a34
cballejo <christianballejo@gmail.com>
2023-10-27 09:48:05 -0300

    correccion diapo 5

---
+++ practicas/Gestion_tablas.R
@@ -0,0 +1,36 @@
+# Gestión de tablas de datos - Encuentro 5
+
+# Datos ordenados
+
+# Pivoteos
+
+# Uniones de tablas
+
+# Transformación 
+
+# Filtro
+
+# Por fila o columna
+
+
+# Gestión de duplicados
+
+# Detección de duplicados
+
+# Eliminación de duplicados por observaciones únicas
+
+# Eliminación de duplicados por recorte de observaciones
+
+# Marcar duplicados
+
+# Guardar o exportar tablas de datos
+
+# Formato texto plano
+
+
+
+
+# Formato Excel
+
+library(openxlsx)
+
